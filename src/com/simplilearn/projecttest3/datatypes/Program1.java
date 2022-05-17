a992d1e9e805db75b27e0c38676fd7108a5cbd4c
TillyWealth <diamondmatilda205@gmail.com>
2022-05-17 12:11:59 -0400

    This is my second commit

---
+++ src/com/simplilearn/projecttest3/datatypes/Program1.java
@@ -0,0 +1,31 @@
+package com.simplilearn.projecttest3.datatypes;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int count=1;
+	       for(int i=1;i<=5;i++) {
+	    	   for(int j=1;j<=5;j++) {
+	    		   if(count<10)
+	    		   {
+	               System.out.print("0");
+	    		   System.out.print(count);
+	    		   System.out.print(" ");
+	    		   count ++;
+	    	   }
+	              else
+	    		   {
+	    	   System.out.print(count);
+	    	   System.out.print(" ");
+	    	   count ++;
+	       }
+	}
+	    	   System.out.println();
+}
+}
+
+
+	}
+
+
---
+++ src/com/simplilearn/projecttest3/datatypes/Program7.java
@@ -0,0 +1,23 @@
+package com.simplilearn.projecttest3.datatypes;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=1;i<=5;i++) {
+			 
+	    	   for(int j=1;j<=5;j++) {
+	    		   if(j%2==0) {
+	               System.out.print("0");
+	    		   System.out.print(" ");
+	    	   }
+	    	   else {
+	    	   System.out.print("1");
+	    	   System.out.print(" ");
+	       }
+		 } 
+		 System.out.println();
+		 }
+	}
+
+}
